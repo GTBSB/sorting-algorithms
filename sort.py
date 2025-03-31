7d8471cfca2ff428edda23c9c72d0ed2187fd754
HardProgerKiller22 <greatbear5@mail.ru>
2025-03-31 14:34:48 +0300

    quick sort

--- sort.py
+++ sort.py
@@ -1,39 +1,24 @@
-def heapify(arr, n, i):
-    largest = i  # Инициализация самого большого элемента как корня
-    left = 2 * i + 1     # Левый дочерний элемент
-    right = 2 * i + 2    # Правый дочерний элемент
-
-    # Если левый дочерний элемент больше корня
-    if left < n and arr[left] > arr[largest]:
-        largest = left
-
-    # Если правый дочерний элемент больше самого большого элемента
-    if right < n and arr[right] > arr[largest]:
-        largest = right
-
-    # Если самый большой элемент не корень
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # Обмен
-
-        # Рекурсивно вызываем heapify для поддерева, которое изменилось
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    # Построение максимальной кучи
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    # Извлечение элементов из кучи
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # Перемещаем текущий корень в конец
-        heapify(arr, i, 0)  # Восстанавливаем кучу
+def quick_sort(arr):
+    # Базовый случай: если массив состоит из одного или ноль элементов, он уже отсортирован
+    if len(arr) <= 1:
+        return arr
+    
+    # Выбираем опорный элемент (обычно берем последний)
+    pivot = arr[-1]
+    
+    # Сортируем элементы, меньшие опорного
+    left = [x for x in arr[:-1] if x <= pivot]
+    
+    # Сортируем элементы, большие опорного
+    right = [x for x in arr[:-1] if x > pivot]
+    
+    # Рекурсивно сортируем обе части и соединяем их с опорным элементом
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
 # Пример использования
-arr = [12, 11, 13, 5, 6, 7]
+arr = [10, 7, 8, 9, 1, 5]
 print("Исходный массив:", arr)
 
-heap_sort(arr)
+sorted_arr = quick_sort(arr)
 
-print("Отсортированный массив:", arr)
+print("Отсортированный массив:", sorted_arr)
